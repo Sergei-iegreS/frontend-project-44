093ab6e1f5ae995e158e7cb5b2debceff4d2aab0
Sergei Kharin <se.kha@mail.ru>
2024-11-06 15:28:08 +0500

    progect

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 export const user = () => {
 
-    const name = readlineSync.question('May I have your name?: ');
+    const name = readlineSync.question('May I have your name?: \n');
 
     console.log("Hello, ", name + "!");
     return name
